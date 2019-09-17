9a5c112200cb545240b382e30ffc7f7c654b7218
penn <pennsong07@gmail.com>
2019-09-17 16:26:26 +0800

    change Base64 lib

--- src/main/java/com/example/ppwebtest/MainController.java
+++ src/main/java/com/example/ppwebtest/MainController.java
@@ -2,9 +2,9 @@ package com.example.ppwebtest;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
 import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @RestController
 public class MainController {
     @RequestMapping("**")
-    public String test(@RequestBody String body) throws Base64DecodingException {
+    public String test(@RequestBody String body) {
         JsonObject jsonObject = new JsonParser().parse(body).getAsJsonObject();
         System.out.println("received request:" + LocalDateTime.now());
         System.out.println(jsonObject);
