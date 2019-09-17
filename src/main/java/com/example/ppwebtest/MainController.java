a1c1bfd3b6fddcb092ba152a3f63af8e09a87986
penn <pennsong07@gmail.com>
2019-09-17 22:30:21 +0800

    switch to @Slf4j

--- src/main/java/com/example/ppwebtest/MainController.java
+++ src/main/java/com/example/ppwebtest/MainController.java
@@ -4,27 +4,26 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-
+@Slf4j
 @RestController
 public class MainController {
     @RequestMapping("pptest")
     public String test1() {
-        System.out.println("pptest");
-
+        log.info("pptest " + LocalDateTime.now());
         return "ok";
     }
 
     @RequestMapping("**")
     public String test(@RequestBody String body) {
         JsonObject jsonObject = new JsonParser().parse(body).getAsJsonObject();
-        System.out.println("received request:" + LocalDateTime.now());
-        System.out.println(jsonObject);
+        log.info("received request:" + body);
 
         // GET HEX
         byte[] hexStringByte = Base64.decode(
@@ -34,7 +33,7 @@ public class MainController {
                         .get("rawData")
                         .getAsString());
         String hexString = HexBin.encode(hexStringByte);
-        System.out.println("hex:" + hexString);
+        log.info("hex:" + hexString);
 
         return "ok";
     }
