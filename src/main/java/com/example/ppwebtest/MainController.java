76e99c99e6286c3735a2d507443a2800387fa674
penn <pennsong07@gmail.com>
2019-09-17 11:24:17 +0800

    add log for request body

--- src/main/java/com/example/ppwebtest/MainController.java
+++ src/main/java/com/example/ppwebtest/MainController.java
@@ -1,5 +1,6 @@
 package com.example.ppwebtest;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,9 @@ import java.time.LocalDateTime;
 @RestController
 public class MainController {
     @RequestMapping("**")
-    public String test() {
+    public String test(@RequestBody String body) {
         System.out.println("received request" + LocalDateTime.now());
+        System.out.println(body);
         return "ok";
     }
 }
