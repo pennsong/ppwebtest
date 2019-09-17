8cad4abe3d4b9dede31a7f41c8ddae29a68f13cf
penn <pennsong07@gmail.com>
2019-09-17 10:45:22 +0800

    add log

--- src/main/java/com/example/ppwebtest/MainController.java
+++ src/main/java/com/example/ppwebtest/MainController.java
@@ -3,10 +3,13 @@ package com.example.ppwebtest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class MainController {
     @RequestMapping("**")
     public String test() {
+        System.out.println("received request" + LocalDateTime.now());
         return "ok";
     }
 }
